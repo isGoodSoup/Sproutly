918e62021d9f6d8bc6b78c4dd9f3faa0499ce5a1
diego <sargent.pointed453@passinbox.com>
2026-03-20 00:07:03 +0100

    add: colors fri 20-03 00:06

---
+++ src/main/java/com/soup/game/service/Colors.java
@@ -0,0 +1,33 @@
+package com.soup.game.service;
+
+import com.soup.game.intf.Service;
+
+import java.awt.*;
+
+/**
+ * <h1>Colors</h1>
+ * A wide range color palette designed to apply
+ * to the GUI layer of the game.
+ */
+@Service
+public class Colors {
+    public static final Color BLACK          = new Color(0, 0, 0);
+    public static final Color RED            = new Color(200, 0, 0);
+    public static final Color GREEN          = new Color(60, 180, 100);
+    public static final Color YELLOW         = new Color(200, 200, 0);
+    public static final Color BLUE           = new Color(0, 100, 200);
+    public static final Color MAGENTA        = new Color(200, 0, 200);
+    public static final Color PURPLE         = new Color(160, 50, 160);
+    public static final Color CYAN           = new Color(0, 220, 255);
+    public static final Color WHITE          = new Color(200, 200, 200);
+
+    public static final Color BRIGHT_BLACK   = new Color(100, 100, 100);
+    public static final Color BRIGHT_RED     = new Color(255, 0, 0);
+    public static final Color BRIGHT_GREEN   = new Color(40, 230, 150);
+    public static final Color BRIGHT_YELLOW  = new Color(255, 255, 0);
+    public static final Color BRIGHT_BLUE    = new Color(0, 155, 255);
+    public static final Color BRIGHT_MAGENTA = new Color(255, 0, 255);
+    public static final Color BRIGHT_PURPLE  = new Color(210, 110, 210);
+    public static final Color BRIGHT_CYAN    = new Color(0, 255, 255);
+    public static final Color BRIGHT_WHITE   = new Color(255, 255, 255);
+}
